bb8fe8f996a08e62f7e8707c20185b1a073f1429
karpovichyan <karpovichjan@gmail.com>
2021-03-30 11:05:16 +0300

    test commit

---
+++ src/main/java/by/intexsoft/testproject/simplecompany/SimplecompanyApplication.java
@@ -0,0 +1,13 @@
+package by.intexsoft.testproject.simplecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplecompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/intexsoft/testproject/simplecompany/SimplecompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package by.intexsoft.testproject.simplecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
