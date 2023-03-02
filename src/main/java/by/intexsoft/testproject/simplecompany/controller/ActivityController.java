f549a30efc511bd10bc26f0023e9c469df80569e
Карпович Ян Геннадьевич (4050718) <yakarpovich@dev.vtb.ru>
2023-03-02 15:16:54 +0300

    123

--- src/main/java/by/intexsoft/testproject/simplecompany/controller/ActivityController.java
+++ src/main/java/by/intexsoft/testproject/simplecompany/controller/ActivityController.java
@@ -2,34 +2,26 @@ package by.intexsoft.testproject.simplecompany.controller;
 
 import by.intexsoft.testproject.simplecompany.dto.ActivityDto;
 import by.intexsoft.testproject.simplecompany.service.ActivityService;
-import by.intexsoft.testproject.simplecompany.service.impl.PayslipServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("activities")
 public class ActivityController {
 
-    private final Logger log = LoggerFactory.getLogger(ActivityController.class);
     private final ActivityService activityService;
-
-    public ActivityController(ActivityService activityService) {
-        this.activityService = activityService;
-    }
 
     @PostMapping
     public ActivityDto create(@Valid @RequestBody ActivityDto activityDto) {
         return activityService.create(activityDto);
-    }
-
-    @GetMapping
-    public List<ActivityDto> getAll() {
-        log.info("");
-        return activityService.getAll();
     }
 
 }
